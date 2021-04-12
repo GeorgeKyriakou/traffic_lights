7b6a7a5b1f87fca8bb7856473f32a51556d331d6
georgios.kyriakou@mirado.com <georgios.kyriakou@mirado.com>
2021-04-12 21:42:20 +0200

    add clearTimeout on destroy. Format Main/index.js

--- components/Main/index.tsx
+++ components/Main/index.tsx
@@ -2,31 +2,46 @@ import React, { useEffect, useContext, useState } from "react";
 import LightsCycleContext from "../../context/traffic-control/context";
 import TrafficLight from "../TrafficLight";
 
-import { MainStyle } from './style'
+import { MainStyle } from "./style";
 
 const Main: React.FC = () => {
-    const { fetchState, state_machine, current_index, nextLightsState } = useContext(LightsCycleContext)
+  const {
+    fetchState,
+    state_machine,
+    current_index,
+    nextLightsState,
+  } = useContext(LightsCycleContext);
 
-    useEffect(() => {
-        console.log('fetching state');
-        fetchState()
-    }, [])
+  useEffect(() => {
+    console.log("fetching state");
+    fetchState();
+  }, []);
 
-    useEffect(() => {
-        setTimeout(nextLightsState, 1000)
-    }, [current_index])
+  useEffect(() => {
+    const tick = setTimeout(nextLightsState, 1000);
+    return () => clearTimeout(tick);
+  }, [current_index]);
 
-    return (<>
-        {state_machine.length && <MainStyle>            
-            <div className="main_wrapper">
-                <TrafficLight lightId={1} state_machine={state_machine} current_index={current_index}>
-                </TrafficLight>
-                <TrafficLight lightId={2} state_machine={state_machine} current_index={current_index}>
-                </TrafficLight>
-            </div>
-        </MainStyle>}
-    </>)
-        ;
-}
+  return (
+    <>
+      {state_machine.length && (
+        <MainStyle>
+          <div className="main_wrapper">
+            <TrafficLight
+              lightId={1}
+              state_machine={state_machine}
+              current_index={current_index}
+            ></TrafficLight>
+            <TrafficLight
+              lightId={2}
+              state_machine={state_machine}
+              current_index={current_index}
+            ></TrafficLight>
+          </div>
+        </MainStyle>
+      )}
+    </>
+  );
+};
 
 export default Main;
